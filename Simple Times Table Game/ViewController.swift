bf2e5d5417a021f0fa7fab6cb67d2208d9a7dc60
Pieter Stragier <pieter@Pieters-iMac.local>
2018-03-06 20:43:49 +0100

    Initial Commit

---
+++ Simple Times Table Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Times Table Game
+//
+//  Created by Pieter Stragier on 06/03/2018.
+//  Copyright © 2018 PWS-apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
