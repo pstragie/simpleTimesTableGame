99ea39a7b40308cf4f2ad357da363f95723c6aca
Pieter Stragier <pieter@Pieters-iMac.local>
2018-04-12 22:38:11 +0200

    First release 1.920

--- Simple Times Table Game/AppDelegate.swift
+++ Simple Times Table Game/AppDelegate.swift
@@ -116,12 +116,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        AppDelegate.AppUtility.lockOrientation(.all)
         let moc = persistentContainer.viewContext
         ViewController().copyUserdataToUserdefaults(managedObjectContext: moc)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        AppDelegate.AppUtility.lockOrientation(.portrait)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -130,6 +132,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        AppDelegate.AppUtility.lockOrientation(.all)
         self.saveContext()
     }
 
